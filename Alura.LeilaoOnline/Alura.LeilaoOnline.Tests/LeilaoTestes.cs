58b5caabbfde70658130499129fc23e334be47aa
felipepatente <felipepatente@yahoo.com.br>
2020-07-07 20:00:27 -0300

    Utilizando a bilioteca XUnit

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -0,0 +1,51 @@
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTestes
+    {
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Mariana", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(fulano, 990);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Gannhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+
+        [Fact]
+        public void LeilaoComApenasUmLance()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Gannhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+    }
+}
