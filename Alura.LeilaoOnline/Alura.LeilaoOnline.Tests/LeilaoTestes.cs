cb86707255b40f5917f199e2e9b4d9bc0670b183
felipepatente <felipepatente@yahoo.com.br>
2020-07-08 19:19:43 -0300

    Escrevendo mais testes

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -6,6 +6,53 @@ namespace Alura.LeilaoOnline.Tests
     public class LeilaoTestes
     {
         [Fact]
+        public void LeilaoComTresClientes()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Mariana", leilao);
+            var beltrano = new Interessada("Beltrano",leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 990);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(beltrano, 1400);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1400;            
+            var valorObtido = leilao.Gannhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+            Assert.Equal(beltrano, leilao.Gannhador.Cliente);
+        }
+
+        [Fact]
+        public void LeilaoComLancesOrdenadosPorValor()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Mariana", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 990);
+            leilao.RecebeLance(fulano, 1000);
+            
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Gannhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
         public void LeilaoComVariosLances()
         {
             //Arrange - cenário
@@ -25,7 +72,6 @@ namespace Alura.LeilaoOnline.Tests
             var valorEsperado = 1000;
             var valorObtido = leilao.Gannhador.Valor;
             Assert.Equal(valorEsperado, valorObtido);
-
         }
 
         [Fact]
